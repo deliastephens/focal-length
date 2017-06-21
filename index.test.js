5e9029cba253c8619d5ea48818209d8e78bb999a
Delia <deliasstephens@gmail.com>
2017-06-21 15:23:54 -0500

    Test fixes

--- index.test.js
+++ index.test.js
@@ -4,6 +4,8 @@ const FOCAL_LENGTH = 35;
 const CROP_FACTOR = 1.5
 const NEW_FOCAL_LENGTH = FOCAL_LENGTH * CROP_FACTOR;
 
-test('adds 1 + 2 to equal 3', () => {
+test(
+  `calculates effective focal length by multiplying crop factor (${CROP_FACTOR} by focal length (${FOCAL_LENGTH}))`, 
+  () => {
   expect(index.calcNewFocalLength(CROP_FACTOR, FOCAL_LENGTH)).toBe(NEW_FOCAL_LENGTH);
 });
