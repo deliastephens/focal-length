39077826e3e750cee8947b9076316b2fd7b0c1bd
Delia <deliasstephens@gmail.com>
2017-06-21 11:33:34 -0500

    Modified dependencies.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+/* Calculates focal length given the crop factor of camera
+  and focal length of lens
+*/
+exports.calcNewFocalLength = function(cropFactor, focalLength) {
+  return cropFactor * focalLength;
 }
